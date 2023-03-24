00ec54d1c24c0d08e55acb100502a79a2de1f4ad
KyleighS <111814352+KyleighS@users.noreply.github.com>
2023-03-23 21:10:17 -0400

    Starting main Menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,36 +7,82 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Transform gameTransform;
     [SerializeField] private Transform piecePrefab;
 
+    private List<Transform> pieces;
     private int emptyLocation;
     public int size = 3;
+    private bool shuffling = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        pieces = new List<Transform>();
         CreateGamePieces(0.04f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //checks for completion
+        if (!shuffling && CheckCompletion())
+        {
+            shuffling = true;
+            StartCoroutine(WaitShuffle(0.5F));
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                for (int i = 0; i < pieces.Count; i++)
+                {
+                    if (pieces[i] == hit.transform)
+                    {
+                        //checks each direction to see if valid move
+                        //break once success
+                        if (SwapIfVaild(i, -size, size)) { break; }
+                        if (SwapIfVaild(i, +size, size)) { break; }
+                        if (SwapIfVaild(i, -1, 0)) { break; }
+                        if (SwapIfVaild(i, +1, size - 1)) { break; }
+                    }
+                }
+            }
+        }
+    }
+
+    //used to stop horzontal moves wrapping
+    private bool SwapIfVaild(int i, int offset, int colCheck)
+    {
+        if (((i % size) != colCheck) && ((i + offset) == emptyLocation))
+        {
+            //swaps the pieces in the game state
+            (pieces[i], pieces[i + offset]) = (pieces[i + offset], pieces[i]);
+            //swap their transform
+            (pieces[i].localPosition, pieces[i + offset].localPosition) = ((pieces[i + offset].localPosition, pieces[i].localPosition));
+
+            emptyLocation = i;
+            return true;
+        }
+        return false;
     }
     //creates the game setup with the size x size pieces
     private void CreateGamePieces(float gapThickness)
     {
-        float width = 1f/size;
+        float width = 1f / size;
 
-        for(int row = 0; row < size; row++)
+        for (int row = 0; row < size; row++)
         {
-            for(int col = 0; col < size; col++)
+            for (int col = 0; col < size; col++)
             {
                 Transform piece = Instantiate(piecePrefab, gameTransform);
+                pieces.Add(piece);
 
                 piece.localPosition = new Vector3(-1 + (2 * width * col) + width, +1 - (2 * width * row) - width, 0);
                 piece.localScale = ((2 * width) - gapThickness) * Vector3.one;
                 piece.name = $"{(row * size) + col}";
 
-                if((row == size -1) && (col == size - 1))
+                if ((row == size - 1) && (col == size - 1))
                 {
                     emptyLocation = (size * size) - 1;
                     piece.gameObject.SetActive(false);
@@ -55,6 +101,59 @@ public class GameManager : MonoBehaviour
 
                     mesh.uv = uv;
                 }
+            }
+        }
+    }
+
+    private bool CheckCompletion()
+    {
+        for (int i = 0; i < pieces.Count; i++)
+        {
+            if (pieces[i].name != $"{i}")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private IEnumerator WaitShuffle(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        Shuffle();
+        shuffling = false;
+    }
+
+    private void Shuffle()
+    {
+        int count = 0;
+        int last = 0;
+
+        while (count < (size * size * size))
+        {
+            //pick a random location
+            int rnd = Random.Range(0, size * size);
+
+            //only thing we forbid is undoing the last move
+            if (rnd == last) { continue; }
+            last = emptyLocation;
+
+            //looks for valid omves
+            if (SwapIfVaild(rnd, -size, size))
+            {
+                count++;
+            }
+            else if (SwapIfVaild(rnd, +size, size))
+            {
+                count++;
+            }
+            else if (SwapIfVaild(rnd, -1, 0))
+            {
+                count++;
+            }
+            else if (SwapIfVaild(rnd, +1, size - 1))
+            {
+                count++;
             }
         }
     }
